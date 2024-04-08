b25ea6d71bb82be3de85685e5a13af4ce7d0213d
pamini <puyagit@gmail.com>
2024-04-08 06:56:18 +0330

    Portal used(ReactDom) for Modal - modal css changed becuz its move out of app div

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,12 @@
 import './modal.css'
+import ReactDom from 'react-dom'
 export default function Modal({children , handleClose}) {
-  return (
+  return ReactDom.createPortal((
     <div className="modal-backdrop">
        <div className="modal">
        {children}
        <button onClick={handleClose}>close</button>
        </div>
     </div>
-  )
+  ), document.body)
 }
