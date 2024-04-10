54e49296057097e9b30120e259f6293971a34616
pamini <puyagit@gmail.com>
2024-04-10 14:26:46 +0330

    controlled inputs --- e41 (s7e3)

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -4,20 +4,27 @@ import React, { useState } from "react";
 export default function NewEventForm() {
   const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
+  const restValues = () =>{
+       setDate('');
+       setTitle('');
+  }
   return (
     <form className="new-event-form">
       <label>
         <span>Event:</span>
-        <input type="text" onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" value={title}
+         onChange={(e) => setTitle(e.target.value)} />
       </label>
       <label>
         <span>Date</span>
-        <input type="date" onChange={(e) => setDate(e.target.value)} />
+        <input type="date" value={date}
+        onChange={(e) => setDate(e.target.value)} />
       </label>
       <button>Submit..</button>
       <p>
         title: {title} - date: {date}
       </p>
+      <p className="reset" onClick={restValues}>reset</p>
     </form>
   );
 }
