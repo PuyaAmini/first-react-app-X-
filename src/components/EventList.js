548663fe7cacc053187f04bc8e8bea9b73753a89
pamini <puyagit@gmail.com>
2024-04-11 13:41:25 +0330

    select boxes added - e45 [s7 e7 ]

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -6,6 +6,7 @@ export default function EventList({events , handleClick}) {
       {events.map((event , index) =>(
       <div className={styles.card}  key = {event.id}>
         <h3>{index} - {event.title}</h3>
+        <p>{event.location} - {event.date}</p>
         <button onClick={()=> handleClick(event.id)}>delete Event</button>
       </div> ))}   
     </div>
--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,29 +1,25 @@
 import "./NewEventForm.css";
-import React, { useState , useRef } from "react";
+import React, { useState} from "react";
 
 export default function NewEventForm({addEvent}) {
-  // const [title, setTitle] = useState("");
-  // const [date, setDate] = useState("");
-  const title = useRef();
-  const date = useRef()
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [location , setLocation] = useState('MARS')
 
   const resetValues = () =>{
-      //  setDate('');
-      //  setTitle('');
-      date.current.value = ''
-      title.current.value = ''
+       setDate('');
+       setTitle('');
+       setLocation('MARS')
   }
 
   // submit func:
   const handleSubmit = (e) => {
      e.preventDefault()
      const event = {
-      //  title: title,
-      //  date: date,
-      title:title.current.value,
-      date:date.current.value,
-
-      id : Math.floor(Math.random() * 100000) //random id generation
+       title: title,
+       date: date,
+       location: location,
+       id : Math.floor(Math.random() * 100000) //random id generation
      }
      addEvent(event)
      resetValues()
@@ -35,17 +31,26 @@ export default function NewEventForm({addEvent}) {
       <label>
         <span>Event:</span>
         <input type="text" 
-        // value={title} onChange={(e) => setTitle(e.target.value)} 
-         ref={title}/>
+        value={title} onChange={(e) => setTitle(e.target.value)} />
       </label>
       <label>
         <span>Date</span>
         <input type="date" 
-        // value={date} onChange={(e) => setDate(e.target.value)}
-         ref={date} />
+        value={date} onChange={(e) => setDate(e.target.value)}/>
       </label>
+      
       <button>Submit..</button>
-
+    <label>
+      <span>location: </span>
+      <select onChange={(e) => setLocation(e.target.value)}>
+        <option value={'london'}>"London"</option>
+        <option value={'washington'}>"Washington"</option>
+        <option value={'tehran'}>"Tehran"</option>
+        <option value={'moscow'}>"Moscow"</option>
+        <option value={'tokyo'}>"Tokyo"</option>
+        <option value={'beijing'}>"Beijing"</option>
+      </select>
+    </label>
     </form>
   );
 }
