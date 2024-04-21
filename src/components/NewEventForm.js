16875f55e8bbfde9f8fcef857a8f5fa14053db0e
pamini <puyagit@gmail.com>
2024-04-21 14:48:10 +0330

    [*retrain] controlled inputs (resetable inputs)

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -3,20 +3,27 @@ import "./NewEventForm.css"
 export default function NewEventForm() {
        const [title, setTitle] = useState('')
        const [date, setDate] = useState('')
+       const resetValues = () => {
+              setDate('');
+              setTitle('');
+       }
        return (
               <form className='new-event-form'>
                      <label>
                             <span>Event:</span>
                             <input type='text'
+                                   value={title}
                                    onChange={e => setTitle(e.target.value)} />
                      </label>
                      <label>
                             <span>Date</span>
                             <input type='date'
+                                   value={date}
                                    onChange={e => setDate(e.target.value)} />
                      </label>
                      <button>Add..</button>
                      <p>title : {title} - date : {date}</p>
+                     <p className='reset' onClick={resetValues}>reset</p>
               </form>
        )
 }
